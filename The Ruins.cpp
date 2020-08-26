f16bf2bb0e09c1e7cebaa34b54ad84f4e14e74b1
Otaku-samaIII <69971976+Octoleet-Dev@users.noreply.github.com>
2020-08-26 15:39:32 +0100

    Create The Ruins.cpp

---
+++ The Ruins.cpp
@@ -0,0 +1,18 @@
+//warning there is a virus inside this code
+//the secret number is 2.08008382
+int math = 999
+double secretcode = 2.08008382;
+int main () {
+
+std::cout << "Warning, this game contains mature content, if you are under the age of 13, please seek parental guidance.\n";
+std::cout << "This game is not for children.\n";
+std::cout << "Within the code there is a secret number as a comment, please enter the code here: \n";
+std::cin >> math
+if (math = secretcode) {
+std::cout << "errorr\n";
+}
+else {
+std::cout << " shh, its me, senpai the code is a three route of nine, im not telling the answer, senpai, your in the wrong file\n";
+std:;cout << " come and visit me in my file, this code is... shared by too many people, they will find me,\n";
+std::cout << "There is an error in the file, please restart the file and DONOTTRYAGAIN\n";
+}
